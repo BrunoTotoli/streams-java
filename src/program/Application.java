4767d0f23a00ea49efcef443df77c855dfa2df58
Bruno Totoli <santosbruno101062@gmail.com>
2022-04-07 16:10:31 -0300

    Streams

--- src/program/Application.java
+++ src/program/Application.java
@@ -3,9 +3,7 @@ package program;
 import entities.Gender;
 import entities.Person;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Application {
@@ -37,6 +35,34 @@ public class Application {
         boolean allMatch = peoples.stream()
                 .allMatch(person -> person.getAge() > 5);
         System.out.println(allMatch);
+        //Any match
+        boolean anyMatch = peoples.stream()
+                .anyMatch(person -> person.getAge() > 121);
+        System.out.println(anyMatch);
+        //None match
+        boolean noneMatch = peoples.stream()
+                .noneMatch(person -> person.getName().equals("Antonio"));
+        System.out.println(noneMatch);
+        // Max
+        peoples.stream()
+                .max(Comparator.comparing(Person::getAge))
+                .ifPresent(System.out::println);
+        // Min
+        peoples.stream()
+                .min(Comparator.comparing(Person::getAge))
+                .ifPresent(System.out::println);
+        // Group
+        Map<Gender, List<Person>> mapGender = peoples.stream()
+                .collect(Collectors.groupingBy(Person::getGender));
+        mapGender.forEach((gender, people) -> {
+            System.out.println(gender);
+            people.forEach(System.out::println);
+        });
+        Optional<String> oldFemale = peoples.stream()
+                .filter(person -> person.getGender().equals(Gender.FEMALE))
+                .min(Comparator.comparing(Person::getAge))
+                .map(Person::getName);
+        oldFemale.ifPresent(System.out::println);
     }
 
 
